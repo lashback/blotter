71ad360b4b91c030dcc75c5514edc11e36975d3c
Nathaniel Lash <lash2@illinois.edu>
2014-05-05 17:44:47 -0500

    City parser improved

--- blotter/apps/incidents/management/commands/geocode.py
+++ blotter/apps/incidents/management/commands/geocode.py
@@ -7,14 +7,14 @@ from django.contrib.gis.geos import GEOSGeometry
 from settings.common import SITE_ROOT
 from omgeo import Geocoder
 from streetaddress import StreetAddressParser
-
+import math
 
 class Command(BaseCommand):
 	help = 'Uses OMGeocoder to geocode addresses and gives you results for the stragglers'
 	
 	def handle(self, *args, **options):
-		parser = StreetAddressParser()
-		try:
+			parser = StreetAddressParser()
+		#try:
 			g = Geocoder([
 				['omgeo.services.Bing',{
 					'settings':{
@@ -23,32 +23,43 @@ class Command(BaseCommand):
 				}],
 			])
 
-			for s in Location.objects.filter(point_location__isnull=True):
+			for location in Location.objects.filter(point_location__isnull=True):
+				string = location.address.strip()
 
-				block_pattern = re.compile('BLOCK OF')
-				cleaned_address = re.sub(block_pattern,'', s.address)
-				if s.agency:
-					address_string = cleaned_address + ", " + s.agency.name + " IL"
-				else:
-					address_string = cleaned_address + ", Champaign, IL"
-				print address_string
-
-					#homeless = address_string.find('HOMELESS')
-					#print homeless
+				if location.city is not None:
+					string = string + ", " + location.city
 				
-				if len(address_string) > 0:
-					geocode_results = g.geocode(address_string,True)
+				else:
+					if location.agency:
+						string = string + ", " + location.agency.name + " IL"
+					else:
+						string = string + ", Champaign, IL"
+				
+				block_pattern = re.compile('BLOCK OF')
+				cleaned_address = re.sub(block_pattern,'', string)
+				print cleaned_address
+				
+				if location.intersection_indicator:
+					slash = re.compile('/')
+					string = re.sub(slash,'at', string)
+					print string
+
+				if len(location.address.strip()) > 0:
+					geocode_results = g.geocode(cleaned_address,True)
 					geocode_candidates = geocode_results['candidates']
 					for c in geocode_candidates:
-						if c.confidence == 'High':
-							s.point_location = GEOSGeometry('POINT(%s %s)' % (c.x, c.y,),4326)
-							s.save()
-							self.stdout.write("Location found: %s %s\n" % (c.x, c.y,))
+
+						if math.floor(c.x) == -89.0 and math.floor(c.y) == 40.0:
+							print "within acceptable ranges"
+							if c.confidence == 'High':
+								location.point_location = GEOSGeometry('POINT(%s %s)' % (c.x, c.y,),4326)
+								location.save()
+								self.stdout.write("Location found: %s %s\n" % (c.x, c.y,))
 					
-					if not s.point_location:
-						self.stdout.write("No good geocode found for %s \n" % (address_string))
-					s.attempted = True
-					s.save()
-		except (RuntimeError, TypeError, NameError):
-			print "whoopsie"
-			
+					if not location.point_location:
+						self.stdout.write("No good geocode found for %s \n" % (cleaned_address))
+					#location.attempted = True				
+					location.save()
+
+		#except (RuntimeError, TypeError, NameError):
+		#	print "whoopsie"
